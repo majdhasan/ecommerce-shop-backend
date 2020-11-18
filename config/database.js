2e319160321e99d3ac0031552517f7658abce967
Majd Hasan <majd.hasan@aperto.com>
2020-11-19 00:19:16 +0100

    change database name

--- config/database.js
+++ config/database.js
@@ -4,7 +4,7 @@ module.exports = ({ env }) => ({
     default: {
       connector: "mongoose",
       settings: {
-        uri: `mongodb+srv://${env("MONGODBUSER")}:${env("MONGODBPASS")}@cluster0.28jr0.mongodb.net/ecommerce?retryWrites=true&w=majority`,
+        uri: `mongodb+srv://${env("MONGODBUSER")}:${env("MONGODBPASS")}@cluster0.28jr0.mongodb.net/ecommerce-shop?retryWrites=true&w=majority`,
       },
       options: {
         ssl: true,
