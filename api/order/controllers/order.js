a630c1a44f3ca2a04b17c1648ffca36a160a15ec
Majd Hasan <majd.hasan@aperto.com>
2020-11-20 19:26:02 +0100

    add setOrderAsPaid endpoint

--- api/order/controllers/order.js
+++ api/order/controllers/order.js
@@ -5,4 +5,15 @@
  * to customize this controller
  */
 
-module.exports = {};
+module.exports = {
+
+    async setOrderAsPaid(ctx){
+
+        const {id} = ctx.params;
+        const order = await strapi.services.order.findOne({id});
+        await strapi.services.order.update({id : order.id} , {status: "paid"} )
+        return {message: "success"}
+    }
+
+
+};
